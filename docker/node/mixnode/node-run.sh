03e75232add747c3c1997197b880a22aadfb38fd
prodasl <prodasl@hotmail.com>
2024-06-17 20:25:33 -0500

    Changes to add a prompt to the node run script Changes to eliminate curl progress bar output

--- docker/node/mixnode/node-run.sh
+++ docker/node/mixnode/node-run.sh
@@ -1,4 +1,18 @@
 #/bin/bash
-nym-node run --id $1 --init-only --mode mixnode --verloc-bind-address 0.0.0.0:1790 --public-ips "$(curl -4 https://ifconfig.me)" &> /dev/null
 
-nohup nym-node run --id $1 --mode mixnode --verloc-bind-address 0.0.0.0:1790 --public-ips "$(curl -4 https://ifconfig.me)" &> /dev/null &
+# Prompt the user to enter the node id
+echo "Please enter the ID you want to set to the node: "
+# Get the data from the user input
+read node_id
+# Validate input has value
+if [ -z "$node_id" ]
+then
+    echo "ERROR: The node ID is mandatory"
+    exit 0
+fi
+# Initialize the node
+nym-node run --id $node_id --init-only --mode mixnode --verloc-bind-address 0.0.0.0:1790 --public-ips "$(curl -4 https://ifconfig.me 2>/dev/null)" &> /dev/null
+# Run the node
+nohup nym-node run --id $node_id --mode mixnode --verloc-bind-address 0.0.0.0:1790 --public-ips "$(curl -4 https://ifconfig.me 2>/dev/nul)" &> /dev/null &
+# Show the node details
+nym-node node-details --id $node_id
