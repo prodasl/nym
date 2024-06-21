e1b3a7110accbfb296ba1ac2296de286d3c18026
prodasl <prodasl@hotmail.com>
2024-06-21 16:25:28 -0500

    Automation of the setup procedure and translated to set an environment variable only in the docker run command phase

--- docker/node/mixnode/node-run.sh
+++ docker/node/mixnode/node-run.sh
@@ -1,18 +1,17 @@
 #/bin/bash
 
-# Prompt the user to enter the node id
-echo "Please enter the ID you want to set to the node: "
-# Get the data from the user input
-read node_id
 # Validate input has value
-if [ -z "$node_id" ]
+if [ -z "$NODE_ID" ]
 then
-    echo "ERROR: The node ID is mandatory"
+    echo "ERROR: The environment variable NODE_ID must be set in the docker run command"
     exit 0
 fi
+
+# run the node
 # Initialize the node
-nym-node run --id $node_id --init-only --mode mixnode --verloc-bind-address 0.0.0.0:1790 --public-ips "$(curl -4 https://ifconfig.me 2>/dev/null)" &> /dev/null
-# Run the node
-nohup nym-node run --id $node_id --mode mixnode --verloc-bind-address 0.0.0.0:1790 --public-ips "$(curl -4 https://ifconfig.me 2>/dev/nul)" &> /dev/null &
-# Show the node details
-nym-node node-details --id $node_id
+CONFIG_FILE="/root/.nym/nym-nodes/"$NODE_ID"/config/config.toml"
+if [ ! -f "$CONFIG_FILE" ]; then
+    nohup nym-node run --id $NODE_ID --init-only --mode mixnode --verloc-bind-address 0.0.0.0:1790 --public-ips "$(curl -4 https://ifconfig.me 2>/dev/null)" &> /dev/null
+fi
+
+nym-node run --mode mixnode --id $NODE_ID --deny-init
