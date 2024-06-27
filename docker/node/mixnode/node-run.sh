30e99709ba5ffdc2d23dd76a58c8685c191abf3a
prodasl <prodasl@hotmail.com>
2024-06-27 13:25:07 -0500

    1. Changes to generate a random ID for the node    if it is provided in the run command 2. Changes to run the nym-node version 2.1 Added --accept-operator-terms-and-conditions     flag

--- docker/node/mixnode/node-run.sh
+++ docker/node/mixnode/node-run.sh
@@ -3,15 +3,15 @@
 # Validate input has value
 if [ -z "$NODE_ID" ]
 then
-    echo "ERROR: The environment variable NODE_ID must be set in the docker run command"
-    exit 0
+    NODE_ID=$(shuf -n 1 /usr/share/dict/words | sed "s/'//g")
+    echo "NODE_ID=$NODE_ID" >> ~/.bashrc
 fi
 
 # run the node
 # Initialize the node
 CONFIG_FILE="/root/.nym/nym-nodes/"$NODE_ID"/config/config.toml"
 if [ ! -f "$CONFIG_FILE" ]; then
-    nohup nym-node run --id $NODE_ID --init-only --mode mixnode --verloc-bind-address 0.0.0.0:1790 --public-ips "$(curl -4 https://ifconfig.me 2>/dev/null)" &> /dev/null
+    nohup nym-node run --id $NODE_ID --init-only --mode mixnode --verloc-bind-address 0.0.0.0:1790 --public-ips "$(curl -4 https://ifconfig.me 2>/dev/null)" --accept-operator-terms-and-conditions &> /dev/null
 fi
 
-nym-node run --mode mixnode --id $NODE_ID --deny-init
+nym-node run --mode mixnode --id $NODE_ID --deny-init --accept-operator-terms-and-conditions
